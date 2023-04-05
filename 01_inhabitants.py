71617ce2cbdec9b788e5db7bd35431e68581d299
taus232323 <tayc13@list.ru>
2023-04-05 10:52:52 +0300

    Initial commit

---
+++ 01_inhabitants.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Вывести на консоль жителей комнат (модули room_1 и room_2)
+# Формат: В комнате room_1 живут: ...
+
+from room_1 import folks as room1_folks
+from room_2 import folks as room2_folks
+
+print('В комнате room_1 живут: ', room1_folks)
+print('В комнате room_2 живут: ', room2_folks)
+
+
+
