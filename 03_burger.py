40c9aaccb1f481effdd545a0e77cf2421e3537da
taus232323 <tayc13@list.ru>
2023-04-05 11:35:46 +0300

    exercise 3 have completed

--- 03_burger.py
+++ 03_burger.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import my_burger
 # Создать модуль my_burger. В нем определить функции добавления инградиентов:
 #  - булочки
 #  - котлеты
@@ -15,4 +15,12 @@
 # Создать рецепт своего бургера, по вашему вкусу.
 # Если не хватает инградиентов - создать соответствующие функции в модуле my_burger
 
-# TODO здесь ваш код
+print(my_burger.bum1())
+print(my_burger.cheese())
+print(my_burger.rissole())
+print(my_burger.sauce())
+print(my_burger.tomatoes())
+print(my_burger.onion())
+print(my_burger.letucce())
+print(my_burger.sauce())
+print(my_burger.bum2())
