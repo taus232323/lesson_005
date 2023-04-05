18a4b8c8f062a8a1c7cd60015ad24ec4857bbc6a
taus232323 <tayc13@list.ru>
2023-04-05 11:10:48 +0300

    exercise 2 have completed

--- 02_district.py
+++ 02_district.py
@@ -5,6 +5,11 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+from district.central_street.house1.room1 import folks as central_room1
+from district.central_street.house1.room2 import folks as central_room2
+from district.soviet_street.house1.room1 import folks as soviet_room1
+from district.soviet_street.house1.room2 import folks as soviet_room2
 
-
+inhabitants = [str(central_room1), str(central_room2), str(soviet_room1), str(soviet_room2)]
+inhabitants_str = ','.join(inhabitants)
+print('На районе живут: ', inhabitants_str)
